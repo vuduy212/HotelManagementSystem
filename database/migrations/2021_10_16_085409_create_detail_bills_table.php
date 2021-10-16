b28bae2d2dd819b6a12c06e7d3e9b1aa12a79f2f
Duy Vu <vuduy0212@gmail.com>
2021-10-16 16:40:10 +0700

    create migration

---
+++ database/migrations/2021_10_16_085409_create_detail_bills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_bills', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_id');
+            $table->string('bill_id');
+            $table->date('start_at');
+            $table->date('finish_at');
+            $table->integer('detail_bill_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_bills');
+    }
+}
