eec017698585fc264c96298bc9b8495f93d129db
Duy Vu <vuduy0212@gmail.com>
2021-10-20 17:13:16 +0700

    create model, controller and seeder

---
+++ app/Models/RoomCategories.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RoomCategories extends Model
+{
+    use HasFactory;
+}
