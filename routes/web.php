839738c4c6f88915e1d865580060ec5be85c1340
Duy Vu <vuduy0212@gmail.com>
2021-10-22 01:02:18 +0700

    create route for RoomCategories, create index method in RoomCategoriesController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomCategoriesController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,19 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*------ Admin ------*/
+Route::prefix('admin')->middleware('auth')->group(function () {
+
+    /*------ Route RoomCategory ------*/
+    Route::prefix('categories')->name('categories.')->group(function () {
+        $class = RoomCategoriesController::class;
+        Route::get('/', [$class, 'index'])->name('index');
+        Route::get('/create', [$class, 'create'])->name('create');
+        Route::post('/', [$class, 'store'])->name('store');
+        Route::get('/{user}', [$class, 'show'])->name('show');
+        Route::put('/{user}', [$class, 'update'])->name('update');
+        Route::delete('/{user}', [$class, 'destroy'])->name('destroy');
+        Route::get('/{user}/edit', [$class, 'edit'])->name('edit');
+    });
+});
