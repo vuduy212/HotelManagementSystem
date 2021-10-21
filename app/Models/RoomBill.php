0552b097308f843e86b4f2e045660d9ef885c34b
Duy Vu <vuduy0212@gmail.com>
2021-10-22 00:59:39 +0700

    add filltable for model

--- app/Models/RoomBill.php
+++ app/Models/RoomBill.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class RoomBill extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'client_name',
+        'ID_card',
+        'phone',
+        'email',
+        'address',
+        'payment_method',
+        'total_money',
+        'bill_status',
+        'created_at'
+    ];
 }
