13c1cafbf7e36e5768cc4dc3ea032619a33de2a4
Duy Vu <vuduy0212@gmail.com>
2021-10-24 02:41:29 +0700

    crud Room Status

--- app/Http/Controllers/RoomStatusController.php
+++ app/Http/Controllers/RoomStatusController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
 use App\Models\RoomStatus;
 use Illuminate\Http\Request;
 
@@ -33,7 +34,9 @@ class RoomStatusController extends Controller
      */
     public function create()
     {
-        //
+        $rooms = Room::all();
+        $status = RoomStatus::all();
+        return view('AdminPage.room_statuses.create', compact('rooms', 'status'));
     }
 
     /**
@@ -42,53 +45,69 @@ class RoomStatusController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
     public function store(Request $request)
     {
-        //
+        $this->roomStatus->saveStatus($request);
+        return redirect(route('status.index'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\RoomStatus  $roomStatus
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function show(RoomStatus $roomStatus)
     {
-        //
+        return view('AdminPage.room_statuses.show')->with([
+            'roomStatus' => $roomStatus,
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\RoomStatus  $roomStatus
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function edit(RoomStatus $roomStatus)
     {
-        //
+        $rooms = Room::all();
+        //$room1 = RoomStatus::first()->rooms()->where('room_name', 'foo')->first();;
+        return view("AdminPage.room_statuses.edit")->with([
+            'roomStatus' => $roomStatus,
+            'rooms' => $rooms,
+            //'room1' => $room1,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\RoomStatus  $roomStatus
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, RoomStatus $roomStatus)
     {
-        //
+        $roomStatus->time = $request->time;
+        $roomStatus->status = $request->status;
+        $roomStatus->room_name = $request->room_name;
+        $roomStatus->save();
+
+        return redirect()->route('status.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\RoomStatus  $roomStatus
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
     public function destroy(RoomStatus $roomStatus)
     {
-        //
+        $roomStatus->delete();
+        return redirect()->route('status.index');
     }
 }
