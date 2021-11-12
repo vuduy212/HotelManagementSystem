337ff6f3eeffe2e80142ecb800ba83e0e814d982
Duy Vu <vuduy0212@gmail.com>
2021-11-13 00:46:33 +0700

    add image for room categories seeder

--- database/seeders/RoomCategoriesSeeder.php
+++ database/seeders/RoomCategoriesSeeder.php
@@ -14,16 +14,46 @@ class RoomCategoriesSeeder extends Seeder
      */
     public function run()
     {
-        $vip = RoomCategories::updateOrCreate(
-            ['category_name' => 'VIP'],
-            ['description' => 'This is VIP Room'],
-            ['price' => 1000.00]
-        );
+        $vip1 = RoomCategories::updateOrCreate([
+            'category_name' => 'VIP1',
+            'description' => 'This is VIP Room for one people',
+            'price' => 1000.00,
+            'images' => 'vip1.jpg'
+        ]);
 
-        $normal = RoomCategories::updateOrCreate(
-            ['category_name' => 'NORMAL'],
-            ['description' => 'This is Normal Room'],
-            ['price' => 500.00]
-        );
+        $vip2 = RoomCategories::updateOrCreate([
+            'category_name' => 'VIP2',
+            'description' => 'This is VIP Room for two people',
+            'price' => 1000.00,
+            'images' => 'vip2.png'
+        ]);
+
+        $vip4 = RoomCategories::updateOrCreate([
+            'category_name' => 'VIP4',
+            'description' => 'This is VIP Room for 4 people ',
+            'price' => 1000.00,
+            'images' => 'vip4.jpeg'
+        ]);
+
+        $normal1 = RoomCategories::updateOrCreate([
+            'category_name' => 'NORMAL1',
+            'description' => 'This is Normal Room for one people',
+            'price' => 500.00,
+            'images' => 'normal1.jpg'
+        ]);
+
+        $normal2 = RoomCategories::updateOrCreate([
+            'category_name' => 'NORMAL2',
+            'description' => 'This is Normal Room for two people',
+            'price' => 500.00,
+            'images' => 'normal2.jpg'
+        ]);
+
+        $normal4 = RoomCategories::updateOrCreate([
+            'category_name' => 'NORMAL4',
+            'description' => 'This is Normal Room for 4 people',
+            'price' => 500.00,
+            'images' => 'normal4.jpg'
+        ]);
     }
 }
