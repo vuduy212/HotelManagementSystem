e07a75f181bc5ebd9e7e4ba6f0e2cc661d7468f6
Duy Vu <vuduy0212@gmail.com>
2021-11-13 16:29:08 +0700

    fix room and room status data

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -15,37 +15,37 @@ class RoomSeeder extends Seeder
     public function run()
     {
         $v1_101 = Room::updateOrCreate([
-            'room_name' => 'VIP1_101',
+            'room_name' => '101',
             'description' => 'This is VIP Room for one people - 101',
             'category_id' => '1',
         ]);
 
         $n1_111 = Room::updateOrCreate([
-            'room_name' => 'NORMAL1_101',
+            'room_name' => '111',
             'description' => 'This is Normal Room for one people - 111',
             'category_id' => '4',
         ]);
 
         $v2_201 = Room::updateOrCreate([
-            'room_name' => 'VIP2_101',
+            'room_name' => '201',
             'description' => 'This is VIP Room for 2 people - 201',
             'category_id' => '2',
         ]);
 
         $n2_211 = Room::updateOrCreate([
-            'room_name' => 'NORMAL2_101',
+            'room_name' => '211',
             'description' => 'This is Normal Room for 2 people - 211',
             'category_id' => '5',
         ]);
 
         $v4_301 = Room::updateOrCreate([
-            'room_name' => 'VIP4_101',
+            'room_name' => '301',
             'description' => 'This is VIP Room for 4 people - 301',
             'category_id' => '3',
         ]);
 
         $n4_311 = Room::updateOrCreate([
-            'room_name' => 'NORMAL4_101',
+            'room_name' => '311',
             'description' => 'This is Normal Room for 4 people - 311',
             'category_id' => '6',
         ]);
--- database/seeders/RoomStatusesSeeder.php
+++ database/seeders/RoomStatusesSeeder.php
@@ -14,16 +14,152 @@ class RoomStatusesSeeder extends Seeder
      */
     public function run()
     {
-        $v101_01_01_2021 = RoomStatus::updateOrCreate([
-            'room_name' => 'V101',
+        //ngay dau tien
+        $v1_101_1 = RoomStatus::updateOrCreate([
+            'room_id' => '1',
             'status' => '0',
             'time' => '2021-01-01'
         ]);
 
-        $v101_02_01_2021 = RoomStatus::updateOrCreate([
-            'room_name' => 'V101',
+        $v2_201_1 = RoomStatus::updateOrCreate([
+            'room_id' => '2',
+            'status' => '1',
+            'time' => '2021-01-01'
+        ]);
+
+        $v4_301_1 = RoomStatus::updateOrCreate([
+            'room_id' => '3',
+            'status' => '0',
+            'time' => '2021-01-01'
+        ]);
+
+        $n1_111_1 = RoomStatus::updateOrCreate([
+            'room_id' => '4',
+            'status' => '1',
+            'time' => '2021-01-01'
+        ]);
+
+        $n2_211_1 = RoomStatus::updateOrCreate([
+            'room_id' => '5',
+            'status' => '0',
+            'time' => '2021-01-01'
+        ]);
+
+        $n4_311_1 = RoomStatus::updateOrCreate([
+            'room_id' => '6',
+            'status' => '1',
+            'time' => '2021-01-01'
+        ]);
+
+        //ngay thu 2
+        $v1_101_2 = RoomStatus::updateOrCreate([
+            'room_id' => '1',
             'status' => '1',
             'time' => '2021-01-02'
+        ]);
+
+        $v2_201_2 = RoomStatus::updateOrCreate([
+            'room_id' => '2',
+            'status' => '0',
+            'time' => '2021-01-02'
+        ]);
+
+        $v4_301_2 = RoomStatus::updateOrCreate([
+            'room_id' => '3',
+            'status' => '1',
+            'time' => '2021-01-02'
+        ]);
+
+        $n1_111_2 = RoomStatus::updateOrCreate([
+            'room_id' => '4',
+            'status' => '0',
+            'time' => '2021-01-02'
+        ]);
+
+        $n2_211_2 = RoomStatus::updateOrCreate([
+            'room_id' => '5',
+            'status' => '1',
+            'time' => '2021-01-02'
+        ]);
+
+        $n4_311_2 = RoomStatus::updateOrCreate([
+            'room_id' => '6',
+            'status' => '0',
+            'time' => '2021-01-02'
+        ]);
+
+        //ngay thu 3
+        $v1_101_3 = RoomStatus::updateOrCreate([
+            'room_id' => '1',
+            'status' => '1',
+            'time' => '2021-01-03'
+        ]);
+
+        $v2_201_3 = RoomStatus::updateOrCreate([
+            'room_id' => '2',
+            'status' => '1',
+            'time' => '2021-01-03'
+        ]);
+
+        $v4_301_3 = RoomStatus::updateOrCreate([
+            'room_id' => '3',
+            'status' => '1',
+            'time' => '2021-01-03'
+        ]);
+
+        $n1_111_3 = RoomStatus::updateOrCreate([
+            'room_id' => '4',
+            'status' => '1',
+            'time' => '2021-01-03'
+        ]);
+
+        $n2_211_3 = RoomStatus::updateOrCreate([
+            'room_id' => '5',
+            'status' => '1',
+            'time' => '2021-01-03'
+        ]);
+
+        $n4_311_3 = RoomStatus::updateOrCreate([
+            'room_id' => '6',
+            'status' => '1',
+            'time' => '2021-01-03'
+        ]);
+
+        //ngay thu 4
+        $v1_101_4 = RoomStatus::updateOrCreate([
+            'room_id' => '1',
+            'status' => '0',
+            'time' => '2021-01-04'
+        ]);
+
+        $v2_201_4 = RoomStatus::updateOrCreate([
+            'room_id' => '2',
+            'status' => '0',
+            'time' => '2021-01-04'
+        ]);
+
+        $v4_301_4 = RoomStatus::updateOrCreate([
+            'room_id' => '3',
+            'status' => '0',
+            'time' => '2021-01-04'
+        ]);
+
+        $n1_111_4 = RoomStatus::updateOrCreate([
+            'room_id' => '4',
+            'status' => '0',
+            'time' => '2021-01-04'
+        ]);
+
+        $n2_211_4 = RoomStatus::updateOrCreate([
+            'room_id' => '5',
+            'status' => '0',
+            'time' => '2021-01-04'
+        ]);
+
+        $n4_311_4 = RoomStatus::updateOrCreate([
+            'room_id' => '6',
+            'status' => '0',
+            'time' => '2021-01-04'
         ]);
     }
 }
