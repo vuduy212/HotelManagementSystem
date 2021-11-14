0ec427d89f1e36c798a3e56da4d85cb0f1490241
Duy Vu <vuduy0212@gmail.com>
2021-11-15 03:11:22 +0700

    add request for room order

---
+++ app/Http/Requests/CreateRoomBillRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\StrLengthRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRoomBillRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'client_name' => [
+                'required',
+                'alpha',
+                new StrLengthRule()
+            ],
+            'date_start' => 'required|date_format:"Y-m-d"|after:tomorrow',
+            'date_finish' => 'required|date_format:"Y-m-d"|after_or_equal:date_start',
+            'CMND' => 'digits:12',
+            'phone' => 'digits:10',
+            'email' => 'required|email',
+        ];
+    }
+}
