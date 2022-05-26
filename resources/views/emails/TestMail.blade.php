1d5c47275bababd0d603a5ecfd6f1802502694c9
vsnduy <vsnduy@cmcglobal.vn>
2022-05-26 18:08:21 +0700

    update reservation form to send mail

--- resources/views/emails/TestMail.blade.php
+++ resources/views/emails/TestMail.blade.php
@@ -11,21 +11,45 @@
 <body>
     <h1>{{ $details['title'] }}</h1>
     <h1>{{ $details['body'] }}</h1>
-    <h1>ID: {{ $details['id'] }}</h1>
-    <h1>Name: {{ $details['client_name'] }}</h1>
-    <h1>Phone: {{ $details['phone'] }}</h1>
-    <h1>Email: {{ $details['email'] }}</h1>
-    <h1>CMND: {{ $details['cmnd'] }}</h1>
-    <h1>Payment method: {{ $details['payment'] }}</h1>
-    <h1>Room category: {{ $details['category_name'] }}</h1>
-    <h1>Room: {{ $details['room_name'] }}</h1>
-    <h1>Number of adults: {{ $details['number_of_adults'] }}</h1>
-    <h1>Number of children: {{ $details['number_of_children'] }}</h1>
-    <h1>Check-in: {{ $details['checkin'] }}</h1>
-    <h1>Check-out: {{ $details['checkout'] }}</h1>
-    <h1>Price: {{ $details['price'] }}</h1>
-    <h1>Time create: {{ $details['created_at'] }}</h1>
-    <h1>Total time use room:{{ $details['time'] }}</h1>
+
+    {{-- 1. Thong tin khach hang --}}
+    <br>
+    <h2>Client Information</h2>
+    <p>Name: {{ $details['client_name'] }}</p>
+    <p>Phone: {{ $details['phone'] }}</p>
+    <p>Email: {{ $details['email'] }}</p>
+    <p>CMND: {{ $details['cmnd'] }}</p>
+
+    {{-- 2. Thong tin don dat phong --}}
+    {{-- <p>ID: {{ $details['id'] }}</p> --}}
+    <br>
+    <h2>Reservation Information</h2>
+    <p>Room category: {{ $details['category_name'] }}</p>
+    <p>Room: {{ $details['room_name'] }}</p>
+    <p>Number of adults: {{ $details['number_of_adults'] }}</p>
+    <p>Number of children: {{ $details['number_of_children'] }}</p>
+    <p>Check-in: {{ $details['checkin'] }}</p>
+    <p>Check-out: {{ $details['checkout'] }}</p>
+    <p>Time create: {{ $details['created_at'] }}</p>
+
+    {{-- 3. Thong tin don gia --}}
+    <br>
+    <h2>Payment Information</h2>
+    <p>Total time use room: {{ $details['time'] }} day</p>
+    <p>Price: {{ $details['price'] }} $</p>
+    <p>Payment method: {{ $details['payment'] }}</p>
+
+    {{-- 4. Thong tin lien he --}}
+    <br>
+    <h2>Contact information</h2>
+    <p>Hotel: Paradise Tam Dao</p>
+    <p>Address: 3/81/79 Cau Giay, Ha Noi</p>
+    <p>Phone: +84 96 777 5035</p>
+    <p>Email: vuduy0212@gmail.com</p>
+
+    {{-- 5. Loi cam on --}}
+    <br>
+    <h1>Thanks and Best Regards !</h1>
 </body>
 
 </html>
